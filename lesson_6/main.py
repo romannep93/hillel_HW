615f08f2c1ef7ce345e8774ec00e4d9d2e60173e
romannep93 <113896121+romannep93@users.noreply.github.com>
2023-05-02 23:33:20 +0300

    main.py

--- lesson_6/main.py
+++ lesson_6/main.py
@@ -2,7 +2,7 @@ import importlib
 import os
 
 if __name__ == '__main__':
-    module_dir = 'hillel_HW_Git/lesson_6'
+    module_dir = 'lesson_6'
     for file in os.listdir(module_dir):
         if file.endswith('.py') and file.startswith('HW_'):
             task_number = file.split('_')[1].split('.')[0]

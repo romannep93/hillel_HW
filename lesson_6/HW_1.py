9ce0327d5cf5fa6842b103d3fe9d587e93d28597
romannep93 <113896121+romannep93@users.noreply.github.com>
2023-05-02 20:54:39 +0300

    Revert "lesson_6"
    This reverts commit 198ae2bcd740f3e2ba14d187455d49735e8f6c4b.

--- lesson_6/HW_1.py
+++
@@ -1,6 +0,0 @@
-number = int(input('Enter number: '))
-
-if number > 0 and (number & (number-1)) == 0:
-    print("YES")
-else:
-    print("NO")
--- lesson_6/HW_5.py
+++
@@ -1,6 +0,0 @@
-def to_dict(lst):
-    return {elem: elem for elem in lst}
-
-
-lst = input("Enter the list separated by commas: ").split(',')
-print(to_dict(lst))

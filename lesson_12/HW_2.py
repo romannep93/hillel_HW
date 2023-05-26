81cd40f14ce3eb18623ba1235c88af278bf0be9d
romannep93 <113896121+romannep93@users.noreply.github.com>
2023-05-26 17:31:10 +0300

    fix lesson_12.HW_2

--- lesson_12/HW_2.py
+++ lesson_12/HW_2.py
@@ -1,69 +1,83 @@
 import requests
 import json
 
-# Request to SWAPI API to get information about the Millennium Falcon ship
-ship_url = 'https://swapi.dev/api/starships/?search=Millennium Falcon'
-ship_response = requests.get(ship_url)
-ship_data = ship_response.json()
 
-# Checking the availability of data for the Millennium Falcon ship
-ship_results = ship_data['results']
-if len(ship_results) > 0:
-    ship = ship_results[0]
-    ship_name = ship.get('name')
-    ship_max_speed = ship.get('max_atmosphering_speed')
-    ship_class = ship.get('starship_class')
-    ship_pilots = ship.get('pilots', [])
-else:
-    print("Millennium Falcon ship not found.")
-    exit()
+def get_ship_info(ship_name):
 
-# Getting information about the pilots
-pilots_info = []
-for pilot_url in ship_pilots:
+    ship_url = f'https://swapi.dev/api/starships/?search={ship_name}'
+    ship_response = requests.get(ship_url)
+    ship_data = ship_response.json()
+
+    ship_results = ship_data['results']
+    if len(ship_results) > 0:
+        ship = ship_results[0]
+        ship_info = {
+            'name': ship.get('name'),
+            'max_speed': ship.get('max_atmosphering_speed'),
+            'class': ship.get('starship_class'),
+            'pilots': ship.get('pilots', [])
+        }
+        return ship_info
+    else:
+        return None
+
+
+def get_pilot_info(pilot_url):
     pilot_response = requests.get(pilot_url)
     pilot_data = pilot_response.json()
-    pilot_name = pilot_data.get('name')
-    pilot_height = pilot_data.get('height')
-    pilot_mass = pilot_data.get('mass')
-    pilot_homeworld = pilot_data.get('homeworld')
-
-    # Getting information about the pilot's homeworld
-    homeworld_response = requests.get(pilot_homeworld)
-    homeworld_data = homeworld_response.json()
-    homeworld_name = homeworld_data.get('name')
-    homeworld_url = homeworld_data.get('url')
-
     pilot_info = {
-        'name': pilot_name,
-        'height': pilot_height,
-        'weight': pilot_mass,
-        'homeworld': homeworld_name,
-        'homeworld_info_url': homeworld_url
+        'name': pilot_data.get('name'),
+        'height': pilot_data.get('height'),
+        'mass': pilot_data.get('mass'),
+        'homeworld': pilot_data.get('homeworld')
     }
-    pilots_info.append(pilot_info)
+    return pilot_info
 
-# Checking the availability of ship and pilots data
-if ship_name and ship_max_speed and ship_class and pilots_info:
-    # Displaying the information on the screen
+
+def get_homeworld_info(homeworld_url):
+
+    homeworld_response = requests.get(homeworld_url)
+    homeworld_data = homeworld_response.json()
+    homeworld_info = {
+        'name': homeworld_data.get('name'),
+        'url': homeworld_data.get('url')
+    }
+    return homeworld_info
+
+
+def fetch_millennium_falcon_info():
+
+    ship_info = get_ship_info('Millennium Falcon')
+    if ship_info is None:
+        print("Millennium Falcon ship not found.")
+        return
+
+    pilots_info = []
+    for pilot_url in ship_info['pilots']:
+        pilot_info = get_pilot_info(pilot_url)
+        if pilot_info:
+            homeworld_info = get_homeworld_info(pilot_info['homeworld'])
+            pilot_info['homeworld'] = homeworld_info
+            pilots_info.append(pilot_info)
+
     print('Information about the Millennium Falcon ship:')
-    print('Name:', ship_name)
-    print('Max Speed:', ship_max_speed)
-    print('Class:', ship_class)
+    print('Name:', ship_info['name'])
+    print('Max Speed:', ship_info['max_speed'])
+    print('Class:', ship_info['class'])
     print('List of pilots:')
     for pilot in pilots_info:
         print('---')
         for key, value in pilot.items():
             print(key + ':', value)
 
-    # Writing the information to a JSON file
     output_data = {
-        'name': ship_name,
-        'max_speed': ship_max_speed,
-        'class': ship_class,
+        'name': ship_info['name'],
+        'max_speed': ship_info['max_speed'],
+        'class': ship_info['class'],
         'pilots': pilots_info
     }
     with open('millennium_falcon_info.json', 'w') as file:
         json.dump(output_data, file, indent=4)
-else:
-    print("An error occurred while retrieving information about the Millennium Falcon ship or its pilots.")
+
+
+fetch_millennium_falcon_info()
